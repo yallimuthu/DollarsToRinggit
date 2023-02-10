f7ae76a4ad04976e0232a91a2cf0747e2e2a8222
yallimuthu <63921803+yallimuthu@users.noreply.github.com>
2023-02-09 17:07:29 -0800

    Add files via upload

---
+++ DollarsToRinggit.java
@@ -0,0 +1,14 @@
+public class DollarsToRinggit {
+    public static void main(String[] args) {
+        double dollars;
+        double ringgit;
+
+        dollars = 2.00;
+    
+        // convert U.S Dollar to ringgit (This is the current conversion rate)
+        ringgit = (dollars * 4.30);
+
+        System.out.print(
+            dollars + " U.S Dollar is equal to " + ringgit + " Malaysia ringgit.");
+    }    
+}
